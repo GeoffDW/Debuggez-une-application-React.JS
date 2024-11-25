d8948a651ce8164e3776291b6340b4133e797a31
Geoffroy Martin <geoffroy_martin9580@hotmail.fr>
2024-11-25 16:58:31 +0100

    fix index.js Page/Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -13,7 +13,16 @@ import Modal from "../../containers/Modal";
 import { useData } from "../../contexts/DataContext";
 
 const Page = () => {
-  const {last} = useData()
+
+  const { data } = useData();
+  const last = 
+  data && data.event && data.event.length > 0
+  ?
+  data.events[data.events.length - 1]
+  :
+  null;
+
+
   return <>
     <header>
       <Menu />
